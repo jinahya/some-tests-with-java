64f42ff57030689f0671a5aea7f836fdd1b8f6ac
Jin Kwon <onacit@wemakeprice.com>
2019-01-11 19:04:51 +0900

    Still working on GCDs

--- src/test/java/com/github/jinahya/test/math/GcdTest.java
+++ src/test/java/com/github/jinahya/test/math/GcdTest.java
@@ -4,13 +4,13 @@ import org.apache.commons.math3.util.ArithmeticUtils;
 import org.junit.jupiter.api.Test;
 
 import static java.util.concurrent.ThreadLocalRandom.current;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GcdTest {
 
     @Test
     void commons() {
+        // -------------------------------------------------------------------------------------------------------------
         {
             assertThrows(ArithmeticException.class, () -> ArithmeticUtils.gcd(Integer.MIN_VALUE, Integer.MIN_VALUE));
             assertThrows(ArithmeticException.class, () -> ArithmeticUtils.gcd(Integer.MIN_VALUE, 0));
@@ -21,12 +21,34 @@ public class GcdTest {
             while (x == 0 || x == Integer.MIN_VALUE) {
                 x = current().nextInt();
             }
+            assertNotSame(0, x);
+            assertNotSame(Integer.MIN_VALUE, x);
             assertEquals(x, ArithmeticUtils.gcd(x, x));
             assertEquals(x, ArithmeticUtils.gcd(x, 0));
             assertEquals(x, ArithmeticUtils.gcd(0, x));
         }
         {
             assertEquals(0, ArithmeticUtils.gcd(0, 0));
+        }
+        // -------------------------------------------------------------------------------------------------------------
+        {
+            assertThrows(ArithmeticException.class, () -> ArithmeticUtils.gcd(Long.MIN_VALUE, Long.MIN_VALUE));
+            assertThrows(ArithmeticException.class, () -> ArithmeticUtils.gcd(Long.MIN_VALUE, 0));
+            assertThrows(ArithmeticException.class, () -> ArithmeticUtils.gcd(0, Long.MIN_VALUE));
+        }
+        {
+            long x = 0L;
+            while (x == 0L || x == Long.MIN_VALUE) {
+                x = current().nextLong();
+            }
+            assertNotSame(0L, x);
+            assertNotSame(Long.MIN_VALUE, x);
+            assertEquals(x, ArithmeticUtils.gcd(x, x));
+            assertEquals(x, ArithmeticUtils.gcd(x, 0L));
+            assertEquals(x, ArithmeticUtils.gcd(0L, x));
+        }
+        {
+            assertEquals(0L, ArithmeticUtils.gcd(0L, 0L));
         }
     }
 }
